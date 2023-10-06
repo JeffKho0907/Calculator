2ef6080dd1ac686c9d1b17a246b19c1fe9afff18
JeffKho0907 <kho.yinjue@gmail.com>
2023-10-06 17:35:16 +0200

    First

---
+++ calculator.js
@@ -0,0 +1,53 @@
+const buttons = document.querySelectorAll('.buttons');
+const display = document.querySelector('#display');
+let x = [];
+let currentOperand = '';
+buttons.forEach((button)=>{
+    button.addEventListener('click', (e)=> {
+        if(e.target.textContent != "=") {
+            x.push(e.target.textContent);
+            currentOperand = x.join('');
+            display.textContent = currentOperand;
+        
+        } else if(e.target.textContent === "="){
+            const delimiters = /[+\/x-]/;
+            let numArray = currentOperand.split(delimiters);
+            let numbers = numArray.map(Number);
+            console.log(numbers);
+            let operators =[]
+            for(let i = 0; i< currentOperand.length; i++) {
+                if(["x", "/", "+", "-"].includes(currentOperand[i])) {
+                    operators.push(currentOperand[i]); 
+                
+                }
+            };
+            console.log(operators);
+            for(let i = 0; i< operators.length; i++) {
+                if(operators[i]== "x") {
+                    numbers[i+1] = numbers[i] * numbers[i+1];
+                    
+                } else if (operators[i]== "-") {
+                    numbers[i+1] = numbers[i] - numbers[i+1];
+                    
+                } else if (operators[i]== "/") {
+                    numbers[i+1] = numbers[i] / numbers[i+1];
+                    
+                } else if (operators[i]== "+") {
+                    numbers[i+1] = numbers[i] + numbers[i+1];
+                    
+                }
+
+            
+            }
+            
+            console.log(numbers[numbers.length -1]);
+
+        }
+        
+
+    })
+})
+
+
+
+    
